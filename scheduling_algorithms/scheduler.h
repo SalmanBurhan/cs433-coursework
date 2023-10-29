ef3f64cab7486947a902b9f532e03f422b0bb591
Salman Burhan <salmanfburhan@gmail.com>
2023-10-29 14:30:00 -0700

    Updated Header. Beautified.

--- scheduling_algorithms/scheduler.h
+++ scheduling_algorithms/scheduler.h
@@ -1,13 +1,12 @@
 /**
-* Assignment 3: CPU Scheduler
+ * Assignment 3: CPU Scheduler
  * @file scheduler.h
- * @author ??? (TODO: your name)
+ * @author Salman Burhan
  * @brief This is the header file for the base Scheduler class. Specific schedulers, e.g. FCFS, SJF and RR, inherit
  *        this base class.
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
+
 #pragma once
 
 #include <vector>
@@ -17,7 +16,8 @@ using namespace std;
 /**
  * @brief This is the base class for the scheduler.
  */
-class Scheduler {
+class Scheduler
+{
 public:
     /**
      * @brief Construct a new Scheduler object
@@ -32,14 +32,13 @@ public:
      *        It is used to initialize the scheduler.
      * @param process_list The list of processes in the simulation.
      */
-    virtual void init(std::vector<PCB>& process_list) = 0;
+    virtual void init(std::vector<PCB> &process_list) = 0;
 
     /**
      * @brief This function is called once after the simulation ends.
      *        It is used to print out the results of the simulation.
      */
     virtual void print_results() = 0;
-
 
     /**
      * @brief This function simulates the scheduling of processes in the ready queue.
